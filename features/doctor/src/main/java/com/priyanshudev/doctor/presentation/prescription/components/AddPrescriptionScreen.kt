2e4405bd3b804768439a0ef888ecf0e89a2c3a75
priyanshuborole <ppborole1762002@gmail.com>
2024-05-26 14:42:01 +0530

    feat: Add Voice to Text option

--- features/doctor/src/main/java/com/priyanshudev/doctor/presentation/prescription/components/AddPrescriptionScreen.kt
+++ features/doctor/src/main/java/com/priyanshudev/doctor/presentation/prescription/components/AddPrescriptionScreen.kt
@@ -1,8 +1,14 @@
 package com.priyanshudev.doctor.presentation.prescription.components
 
+import android.content.Intent
+import android.speech.RecognizerIntent
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,6 +21,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
@@ -22,6 +29,8 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -31,8 +40,10 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -69,6 +80,41 @@ import java.util.Locale
 
 @Composable
 fun AddPrescriptionScreen() {
+    var symptomsText by remember { mutableStateOf("") }
+    var observationText by remember { mutableStateOf("") }
+    var diagnosisText by remember { mutableStateOf("") }
+
+    val symptomsSpeechRecognizerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == 100 || result.data != null) {
+            val res = result.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            res?.let {
+                symptomsText = it[0]
+            }
+        }
+    }
+    val observationSpeechRecognizerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == 100 || result.data != null) {
+            val res = result.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            res?.let {
+                observationText = it[0]
+            }
+        }
+    }
+    val diagnosisSpeechRecognizerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == 100 || result.data != null) {
+            val res = result.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            res?.let {
+                diagnosisText = it[0]
+            }
+        }
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -77,9 +123,24 @@ fun AddPrescriptionScreen() {
     ) {
         Header()
         DateText()
-        PrescriptionTextFields(textField = "Symptoms")
-        PrescriptionTextFields(textField = "Observation")
-        PrescriptionTextFields(textField = "Diagnosis")
+        PrescriptionTextFields(
+            textField = "Symptoms",
+            symptomsText,
+            { symptomsText = it },
+            { speechToText("Symptoms", symptomsSpeechRecognizerLauncher) }
+        )
+        PrescriptionTextFields(
+            textField = "Observation",
+            observationText,
+            { observationText = it },
+            { speechToText("Observation", observationSpeechRecognizerLauncher) }
+        )
+        PrescriptionTextFields(
+            textField = "Diagnosis",
+            diagnosisText,
+            { diagnosisText = it },
+            { speechToText("Diagnosis", diagnosisSpeechRecognizerLauncher) }
+        )
         AddMedicine()
         CreatePrescriptionButton()
     }
@@ -113,25 +174,43 @@ fun Header() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PrescriptionTextFields(textField: String) {
-    var text by remember { mutableStateOf(TextFieldValue("")) }
-    Column(
+fun PrescriptionTextFields(
+    textField: String,
+    value: String,
+    onValueChanged: (text: String) -> Unit = {},
+    onClickListener: () -> Unit = {}
+) {
+    var text by remember { mutableStateOf(TextFieldValue(value)) }
+    Row(
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
             .padding(start = 16.dp, end = 16.dp, top = 10.dp)
     ) {
         OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = text,
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 8.dp),
+            value = value,
             label = { Text(text = textField) },
             onValueChange = {
-                text = it
+                onValueChanged(it)
             }, shape = RoundedCornerShape(10.dp),
-            colors = TextFieldDefaults.outlinedTextFieldColors(
+            colors = OutlinedTextFieldDefaults.colors(
                 focusedBorderColor = appBlue,
-                unfocusedBorderColor = strokeColor
+                unfocusedBorderColor = strokeColor,
             )
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_mic),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(15.dp)
+                .size(24.dp)
+                .align(Alignment.CenterVertically)
+                .clickable {
+                    onClickListener()
+                }
         )
     }
 }
@@ -659,4 +738,15 @@ fun AddMedicineTable(medicines: List<Medicines>) {
             index++;
         }
     }
+}
+
+fun speechToText(text: String, speechRecognizerLauncher: ActivityResultLauncher<Intent>) {
+    val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+        putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak the $text")
+    }
+    speechRecognizerLauncher.launch(intent)
 }
